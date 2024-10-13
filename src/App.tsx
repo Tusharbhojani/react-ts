ff45b0e9a5867ea2387c1c6fa0247b916424c36a
Tushar Bhojani <bhojani.tushar2012@gmail.com>
2024-10-13 11:37:48 +0530

    add api for infinite scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
+import { useEffect, useRef } from "react";
 import "./App.css";
 import useInfiniteScroll from "./utils/hooks/infiniteScroll";
 
 function App() {
-  const { products, setProducts } = useInfiniteScroll();
+  const ref = useRef(null);
+
+  const { products, setProducts, setPageIndex, isLoadMore, isLoading } =
+    useInfiniteScroll();
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          if (isLoadMore && !isLoading) {
+            setPageIndex((prev) => prev + 1);
+          }
+        }
+      },
+      { threshold: 0.5 }
+    );
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, [ref, isLoading, isLoadMore]);
 
   return (
     <div className="App">
       <div className="grid gap-4 p-5 ">
         {products.length > 0 &&
-          products.map((product: any) => {
+          products.map((product: any, index: number) => {
             const image = product.images[0];
             return (
-              <div className="border shadow rounded">
+              <div key={index} className="border shadow rounded">
                 <p className=" p-3">{product.title}</p>
-                <div className="w-44 mx-auto">
+                <div className="w-44 mx-auto min-h-44">
                   <img src={image} alt="" />
                 </div>
               </div>
             );
           })}
+
+        <div ref={ref} className="border bg-red-400">
+          Load Mode
+        </div>
       </div>
     </div>
   );
--- src/utils/hooks/infiniteScroll.ts
+++ src/utils/hooks/infiniteScroll.ts
@@ -2,24 +2,39 @@ import { useEffect, useState } from "react";
 
 export default function useInfiniteScroll() {
   const [products, setProducts] = useState<any>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [pageIndex, setPageIndex] = useState(1);
+  const [isLoadMore, setIsLoadMore] = useState(true);
+  
 
   async function getPageData() {
-    const url = "https://dummyjson.com/products?limit=10&skip=10";
+    console.log("fetching data page index: " + pageIndex);
+
+    const skip = (pageIndex - 1) * 10;
+    const url = `https://dummyjson.com/products?limit=10&skip=${skip}`;
     const options = { method: "GET", headers: { accept: "application/json" } };
 
     try {
+      setIsLoading(true);
       const response = await fetch(url, options);
       const data = await response.json();
       console.log(data);
-      setProducts(data.products);
+      setProducts((prev: any) => [...prev, ...data.products]);
+
+      if (data.products.length < 10) {
+        setIsLoadMore(false);
+      }
+
     } catch (error) {
       console.error(error);
+    } finally {
+      setIsLoading(false);
     }
   }
 
   useEffect(() => {
     getPageData();
-  }, []);
+  }, [pageIndex]);
 
-  return { products, setProducts };
+  return { products, setProducts, setPageIndex, isLoadMore, isLoading };
 }
